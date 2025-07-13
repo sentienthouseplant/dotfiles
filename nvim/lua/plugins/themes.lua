98391bdefcd2eba38c73808bfa31cb9dc6236f36
Jack Harrington <github@skipmail.sentienthouseplant.com>
2025-07-13 21:00:57 +0100

    Everforest theme

---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,17 @@
+return {
+
+  {
+    "neanias/everforest-nvim",
+    main = "everforest",
+    lazy = false,
+    opts = {
+      background = "hard", -- "soft", "medium", "hard"
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
