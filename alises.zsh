0847ea655906e5ef3818e2d0e58283f58e6e97c9
Jack Harrington <github@skipmail.sentienthouseplant.com>
2025-07-15 22:35:36 +0100

    Added more nvim config and added some words to my dictionary

--- alises.zsh
+++ alises.zsh
@@ -2,5 +2,6 @@ alias e="exit"
 alias ll="eza -aT"
 alias o="open ." # Open the current directory in Finder
 alias clr="clear" # Clear your terminal screen
-alias hm="cd ~"
+alias hm="z ~"
 alias gl='git log --pretty=format:"%h %ad %s" --date=short --all'
+alias nts='z ~/notes'
